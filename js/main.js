855b873c3ac458bb01c5bda8c0681e0452777860
ghatiko <ghatiko_chemako@hotmail.com>
2019-05-18 18:09:30 -0400

    My primero

---
+++ js/main.js
@@ -0,0 +1,12 @@
+
+$(document).ready(function(){
+    //Animacion
+    new WOW().init();
+
+    //smooth-scroll
+    var scroll = new SmoothScroll('a[href*="#"]', {
+        offset: 90
+    });
+})
+  
+
